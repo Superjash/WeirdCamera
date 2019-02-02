01a7ec3af06e43701f067109b15dbb8166dc4a01
Jash <jiang.shaoheng@immomo.com>
2019-02-02 15:54:59 +0800

    partial

--- WeirdCamera/ViewController.swift
+++ WeirdCamera/ViewController.swift
@@ -10,12 +10,27 @@ import UIKit
 import GPUImage
 
 class ViewController: UIViewController {
+    
+    private var previewImageView: GPUImageView!
+    private var camera: GPUImageVideoCamera!
+    private var filter: GPUImageFilter!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
         
+        previewImageView = GPUImageView(frame: view.bounds)
+        view.addSubview(previewImageView)
         
+        camera = GPUImageVideoCamera(sessionPreset: AVCaptureSession.Preset.hd1920x1080.rawValue, cameraPosition: AVCaptureDevice.Position.back)
+        camera.rotateCamera()
+        camera.outputImageOrientation = .portrait
+        camera.addTarget(filter)
+        
+        filter = GPUImageFilter(fragmentShaderFromFile: "")
+        filter.addTarget(previewImageView)
+        
+        camera.startCapture()
     }
 }
 
